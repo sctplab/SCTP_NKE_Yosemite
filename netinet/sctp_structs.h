c5dccfeaf220b2cea33d04e6b979f2f0f13c96c0
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2014-12-04 22:10:44 +0000

    Sync FBSDIDs.

--- netinet/sctp_structs.h
+++ netinet/sctp_structs.h
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_structs.h 275427 2014-12-02 20:29:29Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_structs.h 275483 2014-12-04 21:17:50Z tuexen $");
 #endif
 
 #ifndef _NETINET_SCTP_STRUCTS_H_
