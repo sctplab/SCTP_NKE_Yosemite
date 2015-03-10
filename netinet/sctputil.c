d8719511a1f7388118eb9ba7b921ab939bba26a7
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2015-03-10 22:39:25 +0000

    Sync FBSDID.

--- netinet/sctputil.c
+++ netinet/sctputil.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctputil.c 279859 2015-03-10 19:49:25Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctputil.c 279867 2015-03-10 22:38:10Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
