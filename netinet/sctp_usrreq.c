90ff8243dd3761ee7ab7f402d0d0e9477c8834dc
Michael Tuexen <tuexen@fh-muenster.de>
2015-11-21 17:25:49 +0100

    Sync FBSDID.

--- netinet/sctp_usrreq.c
+++ netinet/sctp_usrreq.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_usrreq.c 291078 2015-11-19 16:46:00Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_usrreq.c 291137 2015-11-21 16:25:09Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
