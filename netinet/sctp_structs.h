68ba32b74644e12911e028758acabcf58fe038c0
Michael Tuexen <tuexen@fh-muenster.de>
2016-01-04 21:08:45 +0100

    Resyn struct sctp_net_route with strcut route.

--- netinet/sctp_structs.h
+++ netinet/sctp_structs.h
@@ -228,12 +228,20 @@ struct iterator_control {
 struct sctp_net_route {
 	sctp_rtentry_t *ro_rt;
 #if defined(__FreeBSD__)
+#if __FreeBSD_version < 1100093
 #if __FreeBSD_version >= 800000
 	void *ro_lle;
 #endif
 #if __FreeBSD_version >= 900000
 	void *ro_ia;
 	int ro_flags;
+#endif
+#else
+	char		*ro_prepend;
+	uint16_t	ro_plen;
+	uint16_t	ro_flags;
+	uint16_t	ro_mtu;
+	uint16_t	spare;
 #endif
 #endif
 #if defined(__APPLE__)
